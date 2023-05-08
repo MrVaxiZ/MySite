85fa727ce238b5a3e4c938d3218a3f5b5f433973
MrVaxiZ <vaxizbe@gmail.com>
2023-05-09 00:19:23 +0200

    TBD: Routes to components and their content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home-page/home-page.component';
+import { EducationComponent } from './_Headers/education/education.component';
+import { SkillsComponent } from './_Headers/skills/skills.component';
+import { WorkExperienceComponent } from './_Headers/work-experience/work-experience.component';
+import { FeaturedProjectsComponent } from './_Headers/featured-projects/featured-projects.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'education', component: EducationComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'work-experience', component: WorkExperienceComponent },
+  { path: 'featured-projects', component: FeaturedProjectsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EducationComponent } from './_Headers/education/education.component';
+import { SkillsComponent } from './_Headers/skills/skills.component';
+import { WorkExperienceComponent } from './_Headers/work-experience/work-experience.component';
+import { FeaturedProjectsComponent } from './_Headers/featured-projects/featured-projects.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    FeaturedProjectsComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
